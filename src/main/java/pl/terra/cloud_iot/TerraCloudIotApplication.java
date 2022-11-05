7f8e0b75b729d47ac85c158e6f312e38d994ba66
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2022-11-05 02:43:02 +0100

    init project and generators work

---
+++ src/main/java/pl/terra/cloud_iot/TerraCloudIotApplication.java
@@ -0,0 +1,13 @@
+package pl.terra.cloud_iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraCloudIotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerraCloudIotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/terra/cloud_iot/TerraCloudIotApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.terra.cloud_iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraCloudIotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
