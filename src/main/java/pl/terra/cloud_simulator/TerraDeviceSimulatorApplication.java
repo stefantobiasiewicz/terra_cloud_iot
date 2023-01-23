9708494b448f4c33a1be81a8b0e09b4406a39d8b
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-01-24 00:33:57 +0100

    Starting simulator on sim profile

--- src/main/java/pl/terra/cloud_simulator/TerraDeviceSimulatorApplication.java
+++ src/main/java/pl/terra/cloud_simulator/TerraDeviceSimulatorApplication.java
@@ -2,12 +2,16 @@ package pl.terra.cloud_simulator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = {"pl.terra.common.*", "pl.terra.cloud_simulator.*"})
 public class TerraDeviceSimulatorApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TerraDeviceSimulatorApplication.class, args);
+        SpringApplication application =
+                new SpringApplication(TerraDeviceSimulatorApplication.class);
+        application.setAdditionalProfiles("sim");
+        application.run(args);
     }
 }
