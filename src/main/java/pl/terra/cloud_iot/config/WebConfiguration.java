a8dd8b31d193fc295a1b371f3ac43117fe9513b2
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2023-02-19 17:52:53 +0100

    web config

--- src/main/java/pl/terra/cloud_iot/config/WebConfiguration.java
+++ src/main/java/pl/terra/cloud_iot/config/WebConfiguration.java
@@ -1,14 +1,17 @@
 package pl.terra.cloud_iot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
-
+@EnableWebSecurity
+public class WebConfiguration extends WebSecurityConfigurerAdapter  {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
     }
 }
